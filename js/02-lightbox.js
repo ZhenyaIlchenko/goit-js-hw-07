e5afcfc6367b4133cdef17f15799c6d1b3941b94
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-10 16:49:26 +0200

    task 2 not all done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemEl = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+<a class="gallery__item" href="${item.original}">
+<img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"/></a></div>`
+  )
+  .join("");
+galleryEl.insertAdjacentHTML("beforeend", galleryItemEl);
+// console.log(galleryEl);
+
+const gallery = new SimpleLightbox(".gallery a");
